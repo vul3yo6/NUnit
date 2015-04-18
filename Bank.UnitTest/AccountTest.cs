d5f2afe0cae98d1a0eecebe7ed885f1478815c38
Ken Tseng <vul3yo6@hotmail.com>
2015-04-19 02:29:45 +0800

    NUnit tutorial basic
    Signed-off-by: Ken Tseng <vul3yo6@hotmail.com>

---
+++ Bank.UnitTest/AccountTest.cs
@@ -0,0 +1,72 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank.UnitTest
+{
+    [TestFixture]
+    public class AccountTest
+    {
+        Account source;
+        Account destination;
+
+        [SetUp]
+        public void Init()
+        {
+            source = new Account();
+            source.Deposit(200m);
+
+            destination = new Account();
+            destination.Deposit(150m);
+        }
+
+        [Test]
+        public void TransferFunds()
+        {
+            source.TransferFunds(destination, 100m);
+
+            Assert.AreEqual(250m, destination.Balance);
+            Assert.AreEqual(100m, source.Balance);
+        }
+
+        [Test]
+        [ExpectedException(typeof(InsufficientFundsException))]
+        public void TransferWithInsufficientFunds()
+        {
+            source.TransferFunds(destination, 300m);
+
+            //var ex = Assert.Catch<InsufficientFundsException>(() => source.TransferFunds(destination, 300m));
+            //StringAssert.Contains("Exception", ex.Message);
+        }
+
+        [Test]
+        [Ignore("Decide how to implement transaction management")]
+        public void TransferWithInsufficientFundsAtomicity()
+        {
+            //try
+            //{
+            //    source.TransferFunds(destination, 100m);
+            //}
+            //catch (InsufficientFundsException expected)
+            //{
+            //}
+
+            //Assert.AreEqual(100m, source.Balance);
+            //Assert.AreEqual(250m, destination.Balance);
+
+            try
+            {
+                source.TransferFunds(destination, 300m);
+            }
+            catch (InsufficientFundsException expected)
+            {
+            }
+
+            Assert.AreEqual(200m, source.Balance);
+            Assert.AreEqual(150m, destination.Balance);
+        }
+    }
+}
---
+++ Bank.UnitTest/DemoTest.cs
@@ -0,0 +1,60 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank.UnitTest
+{
+    [TestFixture]
+    public class DemoTest
+    {
+        [Test]
+        public void Add_Input_First_1_Second_2_Return_3()
+        {
+            //arrange
+            Account source = new Account();
+            decimal deposit = 300m;
+            decimal withdraw = 100m;
+            decimal expected = 200m;
+
+            //act
+            decimal actual;
+            source.Deposit(deposit);
+            source.Withdraw(withdraw);
+            actual = source.Balance;
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Assert_AreEqual()
+        {
+            Assert.AreEqual(2, 1+1, "Math is broken");
+
+        }
+
+        [Test]
+        public void Assert_AreSame()
+        {
+            Assert.AreSame(int.Parse("1"), int.Parse("1"), "this test should fail");
+        }
+
+        [Test]
+        [Category("DemoTest")]
+        public void Assert_That()
+        {
+            Assert.That("Kent", Is.StringContaining("ent"));
+        }
+
+        [TestCase("Kent")]
+        [TestCase("Kenny")]
+        public void Assert_AreEqual_TestCase(string name)
+        {
+            bool result = name.Contains("Ken");
+            Assert.True(result);
+        }
+    }
+}
