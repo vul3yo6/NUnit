e77213012e0973a28aeed0ef46c0bf974043ab5d
Ken Tseng <vul3yo6@hotmail.com>
2015-04-21 00:30:12 +0800

    add stub and mock sample
    Signed-off-by: Ken Tseng <vul3yo6@hotmail.com>

---
+++ Bank.UnitTest/LogAnalyzerTest.cs
@@ -0,0 +1,138 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank.UnitTest
+{
+    [TestFixture]
+    public class LogAnalyzerTest
+    {
+        #region Basic
+
+        [TestCase("filewithgoodextension.SLF")]
+        [TestCase("filewithgoodextension.slf")]
+        public void IsValidLogFileName_ValidExtensions_ReturnTrue(string fileName)
+        {
+            //arrange
+            LogAnalyzer log = new LogAnalyzer();
+            bool expected = true;
+
+            //act
+            bool result = log.IsValidLogFileName(fileName);
+
+            //assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException), ExpectedMessage = "filename has to be provided")]
+        public void IsValidLogFileName_EmptyFileName_ThrowException()
+        {
+            //arrange
+            LogAnalyzer log = new LogAnalyzer();
+
+            //act
+            bool result = log.IsValidLogFileName("");
+        }
+
+        [Test]
+        public void IsValidLogFileName_EmptyFileName_Throw()
+        {
+            //arrange
+            LogAnalyzer log = new LogAnalyzer();
+            string expected = "filename has to be provided";
+
+            //act
+            var ex = Assert.Catch<ArgumentException>(() => log.IsValidLogFileName(""));
+
+            //assert
+            //Assert.AreEqual(expected, ex.Message);
+            StringAssert.Contains(expected, ex.Message);
+        }
+
+        [Test]
+        public void IsValidLogFileName_WhenCalled_ChangeWasLastFileNameValid()
+        {
+            //arrange
+            LogAnalyzer log = new LogAnalyzer();
+            bool expected = false;
+
+            //act
+            log.IsValidLogFileName("badname.foo");
+
+            //assert
+            Assert.AreEqual(expected, log.WasLastFileNameValid);
+            //Assert.False(log.WasLastFileNameValid);
+        }
+
+        [Test]
+        public void IsValidLogFileName_WhenCalled_BeforeValid()
+        {
+            //arrange
+            LogAnalyzer log = new LogAnalyzer();
+            bool wasCalled = false;
+
+            //act
+            log.BeforeValid += (o, e) => wasCalled = true;
+            //log.BeforeValid += delegate(o, e){ wasCalled = true;}
+            log.IsValidLogFileName("badname.foo");
+
+            //assert
+            Assert.True(wasCalled);
+        }
+
+        #endregion
+
+        #region Advanced
+
+        [Test]
+        public void LogError_WhenCalled_CallWebService()
+        {
+            ////arrange
+            //FakeWebService service = new FakeWebService();
+            //FakeLogAnalyzer log = new FakeLogAnalyzer(service);
+            //string expected = "service write some error";
+
+            ////act
+            //log.LogError(expected);
+
+            ////assert
+            //StringAssert.Contains(expected, service.LastError);
+        }
+
+        // 建構式注入
+        public class FakeWebService : IWebService
+        {
+            public string LastError { get; set; }
+
+            public void LogError(string message)
+            {
+                LastError = message;
+            }
+        }
+
+        // 抽取與重寫
+        public class FakeLogAnalyzer : LogAnalyzer
+        {
+            //public FakeLogAnalyzer(IWebService service) : base(service) { }
+
+            //protected override IKeyPro GetKeyPro()
+            //{
+            //    return new FakeKeyPro();
+            //}
+        }
+
+        public class FakeKeyPro : IKeyPro
+        {
+            public bool Check()
+            {
+                return true;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Bank/LogAnalyzer.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    public class LogAnalyzer
+    {
+        #region Basic
+
+        public bool WasLastFileNameValid { get; set; }
+
+        public event EventHandler BeforeValid;
+
+        public LogAnalyzer() { }
+
+        public bool IsValidLogFileName(string fileName)
+        {
+            WasLastFileNameValid = false;
+
+            if (string.IsNullOrEmpty(fileName))
+            {
+                throw new ArgumentException("filename has to be provided");
+            }
+
+            // 呼叫事件
+            if (BeforeValid != null)
+            {
+                BeforeValid(this, EventArgs.Empty);
+            }
+
+            if (fileName.EndsWith(".SLF"))
+            {
+                WasLastFileNameValid = true;
+                return true;
+            }
+
+            return false;
+        } 
+
+        #endregion
+
+        // --------------------------------------
+
+        #region Advanced
+
+        //protected IWebService service;
+
+        //public LogAnalyzer(IWebService service)
+        //{
+        //    this.service = service;
+        //}
+
+        protected WebService service = new WebService();
+
+        /// <summary>
+        /// 1.透過抽取與重寫 - 建立KeyPro存根
+        /// 2.透過建構式注入 - 建立WebService模擬對象
+        /// </summary>
+        /// <param name="message"></param>
+        public void LogError(string message)
+        {
+            //var key = GetKeyPro();
+            var key = new KeyPro();
+            if (key.Check())
+            {
+                this.service.LogError(message);
+            }
+        }
+
+        //protected virtual IKeyPro GetKeyPro()
+        //{
+        //    return new KeyPro();
+        //} 
+
+        #endregion
+    }
+
+    // 模擬對象, 可以截取成interface
+    public class WebService //: IWebService
+    {
+        public void LogError(string message) { }
+    }
+
+    public interface IWebService
+    {
+        void LogError(string message);
+    }
+
+    // 存根, 可以截取成interface
+    public class KeyPro //: IKeyPro
+    {
+        public bool Check()
+        {
+            return false;
+        }
+    }
+
+    public interface IKeyPro
+    {
+        bool Check();
+    }
+}
